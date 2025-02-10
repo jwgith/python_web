f7af357098765bf5e32b567d6265ba1acf34aee4
jwh <gmdt1998@naver.com>
2025-02-10 16:20:23 +0900

    5 commit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -24,6 +24,7 @@ class Post(models.Model):
 
     # ewoirjwoeirjoweijr
     # DFDSFASFFGHTGEERE
+    ###erererererer
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=1)
     updated = models.DateTimeField(auto_now=1)
