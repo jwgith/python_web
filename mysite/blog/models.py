cfe6ed35b8aa45e1bcf5d7df7e0f41bbcfae4fd2
DoHyunLee <osolinnn@gmail.com>
2025-02-10 16:24:35 +0900

    test 77

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
     # ewoirjwoeirjoweijr
     # DFDSFASFFGHTGEERE
     # dddddddddddddd
-    # test58787878
+    # hggygygy
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=1)
     updated = models.DateTimeField(auto_now=1)
